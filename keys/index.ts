944e86ca22517399f2e4d465aec68be3f3d3ac26
Abel Shanoj <abelabrahamshanoj0484@gmail.com>
2025-01-27 22:06:51 +0530

    first

---
+++ keys/index.ts
@@ -0,0 +1,8 @@
+// clientIds.ts
+const CLIENT_IDS = {
+    web: '919719835579-u3cigdpgnl6e4shb4r1grc61vphum65o.apps.googleusercontent.com',
+    ios: '919719835579-b495tqvg6pakr0dqcqnfpli70qihoe8h.apps.googleusercontent.com',
+  };
+  
+  export default CLIENT_IDS;
+  
